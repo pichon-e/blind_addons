718537dbcb4cc9173c0b90c2dc5dcee38f2d56f0
Aurélien Grifasi <aurelien.grifasi@gmail.com>
2017-02-10 12:37:25 +0100

    no message

--- borderify.js
+++ borderify.js
@@ -1,3 +1,27 @@
-document.body.style.border = "15px solid red";
+var include = function(url, callback){
+ 
+    /* on crée une balise<script type="text/javascript"></script> */
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+ 
+    /* On fait pointer la balise sur le script qu'on veut charger
+       avec en prime un timestamp pour éviter les problèmes de cache
+    */
+ 
+    script.src = url + '?' + (new Date().getTime());
+ 
+    /* On dit d'exécuter cette fonction une fois que le script est chargé */
+    if (callback) {
+        script.onreadystatechange = callback;
+        script.onload = script.onreadystatechange;
+    }
+ 
+    /* On rajoute la balise script dans le head, ce qui démarre le téléchargement */
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
 
-console.log('toto');
+include('./page-script.js', function() {
+	console.log('page-script')
+})
+
+document.body.style.border = "15px solid red";
---
+++ page-script.js
@@ -0,0 +1,7 @@
+// page-script.js
+
+// var chart = window.myMixedChart;
+var chart = window.myMixedChart.data
+
+// console.log(chart.data);
+console.log(chart);
