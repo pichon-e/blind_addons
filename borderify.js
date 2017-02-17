5bd147630206c211fc4ba48a255061bdb9134c67
pichon-e <pichon_e@epitech.eu>
2017-02-17 13:40:09 +0100

    add directive

--- borderify.js
+++ borderify.js
@@ -1,27 +1,18 @@
-var include = function(url, callback){
- 
-    /* on crée une balise<script type="text/javascript"></script> */
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
- 
-    /* On fait pointer la balise sur le script qu'on veut charger
-       avec en prime un timestamp pour éviter les problèmes de cache
-    */
- 
-    script.src = url + '?' + (new Date().getTime());
- 
-    /* On dit d'exécuter cette fonction une fois que le script est chargé */
-    if (callback) {
-        script.onreadystatechange = callback;
-        script.onload = script.onreadystatechange;
+var blind = false;
+
+document.getElementById("click").addEventListener("click", function(){
+    blind = !blind;
+    if (blind) {
+        document.getElementById("chart1").style.display = 'none';
+        document.getElementById("button1").style.display = 'block';
+        document.getElementById("chart2").style.display = 'none';
+        document.getElementById("button2").style.display = 'block';
     }
- 
-    /* On rajoute la balise script dans le head, ce qui démarre le téléchargement */
-    document.getElementsByTagName('head')[0].appendChild(script);
-}
+    else {
+        document.getElementById("chart1").style.display = 'block';
+        document.getElementById("button1").style.display = 'none';
+        document.getElementById("chart2").style.display = 'block';
+        document.getElementById("button2").style.display = 'none';
+    }
 
-include('./page-script.js', function() {
-	console.log('page-script')
-})
-
-document.body.style.border = "15px solid red";
+});
--- page-script.js
+++ page-script.js
@@ -1,7 +1,15 @@
 // page-script.js
 
 // var chart = window.myMixedChart;
-var chart = window.myMixedChart.data
 
+/* console.log("test");
+
+window.onload = function() {
+    console.log("test");
+    var chart = window.char1.data;
+    document.getElementById("chart1").style.display = 'none';
+}
+
+console.log("test");
 // console.log(chart.data);
-console.log(chart);
+console.log(chart);*/
